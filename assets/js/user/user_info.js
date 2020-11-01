ec2434af820fd97db35965ec362fb52d11c962dd
zhaohongliang1999 <3075814465@qq.com>
2020-11-01 15:28:10 +0800

    使用form_val方法快速为基本资料赋值

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -7,4 +7,21 @@ $(function () {
             }
         }
     })
+
+    initUserInfo()
+    // 获取用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res.data);
+                //调用 form.val() 快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
 })
