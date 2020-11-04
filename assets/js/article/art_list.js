d0e4ec3755d4b97f756a5ec1baf1408231499b20
zhaohongliang1999 <3075814465@qq.com>
2020-11-04 10:44:31 +0800

    实现删除文章的功能

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,7 @@
 $(function () {
     var layer = layui.layer
     var form = layui.form
+    var laypage = layui.laypage
     // 定义美化时间格式的过滤器
     template.defaults.imports.dataFormat = function (date) {
         const dt = new Date(date)
@@ -37,13 +38,10 @@ $(function () {
                     return layer.msg('获取文章列表失败！')
                 }
                 //使用模版引擎渲染
-                var htmlStr = template('tpl-table', {
-                    data: [
-                        { id: 6991, username: "ifer233", nickname: "哈223", email: "ifer233@qq.com", pub_date: 'xxx' }
-                    ]
-                })
+                var htmlStr = template('tpl-table', res)
                 $('tbody').html(htmlStr)
-
+                //调用渲染分页的方法
+                renderPage(res.total)
             }
         })
     }
@@ -76,5 +74,53 @@ $(function () {
         q.state = state
         //根据最新的筛选条件，重新渲染数据
         initTable()
+    })
+    //定义渲染分页的方法
+    function renderPage(total) {
+        //    调用laypage.render()方法来渲染分页的结构
+        laypage.render({
+            elem: 'pagBox', // 分页容器的id
+            count: total, //总数据条数
+            limit: q.pagesize,//每页显示几条数据
+            curr: q.pagenum,//默认被选中的分页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            // 分页发生切换的时候，触发jump回调
+            jump: function (obj, first) {
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+    // 删除文章
+    $('tbody').on('click', '.btn-delete', function () {
+        // 获取删除按钮的个数
+        var len = $('.btn-delete').length
+        //获取到文章的id
+        var id = $(this).attr('data-id')
+        //询问用户是否要删除数据
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+
+                    if (len === 1) {
+                        // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
+                        // 页码值最小必须是 1
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+            layer.close(index)
+        })
     })
 })
